d3e17afd2177d2bcd54cb325ef4598092f1ddc00
Abhishek Pratap Singh <31668873+abhish1ss@users.noreply.github.com>
2023-12-25 21:16:15 +0530

    Adding more output functions

---
+++ Day2/app.js
@@ -0,0 +1,6 @@
+// console.log("Hello world")
+// document.getElementById("demo").innerHTML = 5 + 6;
+// document.write("<h1>this is app.js output</h1>")
+window.alert("Hello World")
+//this is the warning tag
+console.warn("This is the warning text")

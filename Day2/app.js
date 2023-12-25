d33c89359bd8654cb4e27ffa5057e0cf83a5d931
Abhishek Pratap Singh <31668873+abhish1ss@users.noreply.github.com>
2023-12-25 22:08:10 +0530

    Updating Day2 app.js

--- Day2/app.js
+++ Day2/app.js
@@ -1,6 +1,10 @@
 // console.log("Hello world")
 // document.getElementById("demo").innerHTML = 5 + 6;
 // document.write("<h1>this is app.js output</h1>")
-window.alert("Hello World")
+// console.alert("this is a alert")
+// window.alert("Hello World")
 //this is the warning tag
+//
 console.warn("This is the warning text")
+// console.error()
+console.log("Hello this is log text")
